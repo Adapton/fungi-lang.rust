c78840339d49b506cad08c4632875cd06f9a9510
Matthew Hammer <Matthew.Hammer@Colorado.Edu>
2018-01-27 06:28:21 -0700

    travis stuff, slack integration

---
+++ travis.sh
@@ -0,0 +1,8 @@
+cargo build --verbose
+cargo test --verbose -- --nocapture
+#cd eval
+#cargo run --release --example adder
+#cargo run --release --example filter
+#cargo run --release --example to_string
+#cargo run --release --example unique_elms
+#cargo run --release --example quickhull -- -s 10000 -t 250
