a68655244f62818fb54b0e65350a5343378b559a
Ryan Vandersmith <ryanvandersmith@gmail.com>
2018-02-07 05:10:43 +0000

    Add simple trace capturing test case

--- tests/tgt_eval.rs
+++ tests/tgt_eval.rs
@@ -2,10 +2,16 @@
 #[macro_use]
 extern crate iodyn_lang;
 
+extern crate adapton;
 
 use std::rc::Rc;
 use iodyn_lang::tgt_ast::*;
 use iodyn_lang::tgt_eval;
+use iodyn_lang::tgt_vis;
+
+use adapton::engine::manage;
+use adapton::reflect;
+
 
 fn eval_closed_exp(e:Exp) -> tgt_eval::ExpTerm {
     let mut env = vec![];
@@ -16,7 +22,24 @@ fn eval_test_equiv(e1:Exp, e2:Exp) {
     println!("> {:?}\n\n", e1);
     let t1 = eval_closed_exp(e1);
     let t2 = eval_closed_exp(e2);
+    // let (t1, x1) = capture_traces(move ||
+    //     eval_closed_exp(tgt_vis::label_exp(e1.clone(), &mut 0))
+    // );
+    // let (t2, x2) = capture_traces(move ||
+    //     eval_closed_exp(tgt_vis::label_exp(e2.clone(), &mut 0))
+    // );
+    // println!("Traces: {:?}\n\n", x1);
     assert_eq!(t1, t2)
+}
+
+fn capture_traces<F>(f: F) -> (tgt_eval::ExpTerm, Vec<reflect::trace::Trace>)
+where F: Fn() -> tgt_eval::ExpTerm {
+    manage::init_dcg();
+    
+    reflect::dcg_reflect_begin();
+    let term = f();
+    let traces = reflect::dcg_reflect_end();
+    (term, traces)
 }
 
 #[test]
@@ -125,4 +148,21 @@ fn eval_fix () {
         tgt_exp![
             ret 1
         ])
+}
+
+#[test]
+fn trace_simple() {
+    let exp = tgt_exp![
+        let pair  = {ret (1, 2)}
+        let (x,y) = {ret pair}
+        x < y
+    ];
+    
+    let vis_exp = tgt_vis::label_exp(exp, &mut 0);
+    println!("Exp: {:?}\n\n", vis_exp);
+    
+    let (term, traces) = capture_traces(move || eval_closed_exp(vis_exp.clone()));
+    println!("Traces: {:?}\n\n", traces);
+    
+    assert_eq!(traces.len(), 6);
 }
