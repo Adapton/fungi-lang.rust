69c3b2073c1fd0abaefe0beb0a348f0c2eca1e40
Matthew Hammer <Matthew.Hammer@Colorado.Edu>
2018-02-19 10:25:50 -0700

    fix typo in opnat bitype tests

--- tests/bitype.rs
+++ tests/bitype.rs
@@ -100,15 +100,15 @@ fn bitype2() {
         }
 
         let opnat_pair:(
-            Thk[0] 0 (x OpNat x OpNat) -> F (Op2Nat)
+            Thk[0] 0 (x OpNat x OpNat) -> 0 F (Op2Nat)
         ) = {
             ret thunk #xoyo. let (xo,yo) = { ret xoyo }
             match (xo) {
                 _u => { ret inj1 () }
-                x  => match (yo) {
+                x  => { match (yo) {
                     _u => { ret inj1 () }
                     y  => { ret inj2 (x,y) }
-                }
+                }}
             }
         }
 
