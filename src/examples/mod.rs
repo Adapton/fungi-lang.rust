dee0931fd4fa1455c3ff89655cb382ea85380986
Matthew Hammer <Matthew.Hammer@Colorado.Edu>
2018-03-10 07:02:32 -0700

    forgot mod.rs

---
+++ src/examples/mod.rs
@@ -0,0 +1,12 @@
+/*!
+
+Examples of data structures and algorithms in Fungi.
+
+
+*/
+
+/// Find the maximum element in a sequence
+pub mod seq_max;
+
+/// Filter a sequence of elements, producing a new (smaller) sequence
+pub mod seq_filter;
