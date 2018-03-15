0922a67ec064111f522c438e19c456c7b4c26a4e
Matthew Hammer <Matthew.Hammer@Colorado.Edu>
2018-03-15 12:09:49 -0600

    more subtyping examples

--- src/examples/basic_subtyping.rs
+++ src/examples/basic_subtyping.rs
@@ -1,5 +1,48 @@
 #[test]
-pub fn listing0_check2 () { fgi_listing_test![
+pub fn listing0_one () { fgi_listing_test![
+
+    let get_one:(
+        Thk[0] foralli (X,Y,Z):NmSet.
+            0 Bool ->
+            0 Nm[X%Z] ->
+            0 Nm[Y%Z] ->
+        { 0 ; 0 }
+        F Nm[X%Y%Z]
+    ) = {
+        ret thunk #b. #nxz. #nyz.
+            if (b) {
+                ret nxz
+            } else {
+                ret nyz
+            }
+    }
+    ret 0
+]}
+
+#[test]
+pub fn listing0_pair () { fgi_listing_test![
+
+    let get_pair:(
+        Thk[0] foralli (X,Y,Z):NmSet.
+            0 Bool ->
+            0 Nm[X%Z] ->
+            0 Nm[Y%Z] ->
+        { 0 ; 0 }
+        F (x Nm[X%Y%Z]
+           x Nm[X%Y%Z])
+    ) = {
+        ret thunk #b. #nxz. #nyz.
+            if (b) {
+                ret (nxz,nyz)
+            } else {
+                ret (nyz,nxz)
+            }
+    }
+    ret 0
+]}
+
+#[test]
+pub fn listing1_check () { fgi_listing_test![
 
     let put_one:(
         Thk[0] foralli (X,Y,Z):NmSet.
@@ -16,7 +59,7 @@ pub fn listing0_check2 () { fgi_listing_test![
 ]}
 
 #[test]
-pub fn listing0_synth2 () { fgi_listing_test![
+pub fn listing2_synth () { fgi_listing_test![
 
     let put_one:(
         Thk[0] foralli (X,Y,Z):NmSet.
