087f6f46df24c06d08bd8a997651202d7bec4cd3
Matthew Hammer <Matthew.Hammer@Colorado.Edu>
2018-03-23 07:48:05 -0700

    minor: rustdoc for dynamics module

--- src/dynamics.rs
+++ src/dynamics.rs
@@ -19,33 +19,43 @@ use std::rc::Rc;
 /// TODO-Sometime: Prune the environments (using free variables as filters)
 pub type Env = Vec<(String,RtVal)>;
 
-/// Run-time values.  Same as ast_tgt::Val, except that (1) there are
-/// no variables ("closed") and (2) unlike values written by user in
-/// their program, run-time values may contain run-time structures,
-/// such as _actual_ thunks and references, a la `Art`s from Adapton
-/// library.
+/// Run-time values. Compare to [ast::Val](https://docs.rs/fungi-lang/0/fungi_lang/ast/enum.Val.html).
+///
+/// Same as [Val](https://docs.rs/fungi-lang/0/fungi_lang/ast/enum.Val.html),
+/// except that these values are (1) closed and (2) may contain run
+/// time-only structures, from the Adapton entine:
+///
+/// - there are no variables ("closed") and
+///
+/// - unlike values written by user in their program, run-time values
+/// may contain run-time structures, such as _actual_ thunks and
+/// references, a la `Art`s from Adapton library.
 #[derive(Clone,Debug,Eq,PartialEq,Hash)]
 pub enum RtVal {
+    /// Unit value
     Unit,
+    /// Pair of run-time values
     Pair(RtValRec, RtValRec),
+    /// First injection of run-time value
     Inj1(RtValRec),
+    /// Second injection of run-time value
     Inj2(RtValRec),
+    /// Rolled run-time value (with a recursive type)
     Roll(RtValRec),
+    /// Dynamic name term
     NameFn(NameTm),
-    
+    /// Natural number
     Nat(usize),
+    /// String
     Str(String),
-    Bool(bool),
-    
-    /// Special-case thunk values: For implementing fix with environment-passing style
+    /// Boolean
+    Bool(bool),    
+    /// Special-case thunk values: For implementing `fix` with environment-passing style
     ThunkAnon(Env, Exp),
-    
     /// AST Names; we convert to Adapton engine names when we use the engine API
     Name(Name),
-    
     /// Refs from Adapton engine; they each contain a run-time value
-    Ref(engine::Art<RtVal>),
-    
+    Ref(engine::Art<RtVal>),    
     /// Thunks from Adapton engine; they each _evaluate to_ a terminal expression
     Thunk(engine::Art<ExpTerm>),
 }
@@ -56,8 +66,11 @@ pub type RtValRec = Rc<RtVal>;
 /// style, where lambdas are associatd with closing environments.
 #[derive(Clone,Debug,Eq,PartialEq,Hash)]
 pub enum ExpTerm {
+    /// Lambda expression, with a closing environment
     Lam(Env, Var, Rc<Exp>),
+    /// Rust-level lambda function, with Fungi-level argument values
     HostFn(HostEvalFn, Vec<RtVal>),
+    /// Produce the given run-time value
     Ret(RtVal),
 }
 
@@ -65,6 +78,8 @@ pub enum ExpTerm {
 /// Term sub-language (STLC + names).
 #[derive(Clone,Debug,Eq,PartialEq,Hash)]
 pub enum NameTmVal {
+    /// (Closed) name term
     Name(Name),
+    /// (Closed) name function
     Lam(Var,NameTm),
 }
