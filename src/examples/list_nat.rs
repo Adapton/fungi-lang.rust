bca3c553baca99ad041ef75da07e69a7a7aa425a
Matthew Hammer <Matthew.Hammer@Colorado.Edu>
2018-04-27 10:49:31 -0600

    more simple list operations

--- src/examples/list_nat.rs
+++ src/examples/list_nat.rs
@@ -19,7 +19,7 @@ pub fn listing0 () { fgi_listing_test![
     ) = {            
         ret thunk #h.#t. ret roll inj2 (h, t)
     }
-
+   
     let rec map:(
         Thk[0] 0 (Thk[0] 0 Nat -> 0 F Nat) -> 0 List -> 0 F List
     ) = {
@@ -35,13 +35,13 @@ pub fn listing0 () { fgi_listing_test![
     }
 
     let rec filter:(
-        Thk[0] 0 (Thk[0] 0 Nat -> 0 F Bool) -> 0 List -> 0 F List
+        Thk[0] 0 List -> 0 (Thk[0] 0 Nat -> 0 F Bool) -> 0 F List
     ) = {
-        #f.#l. unroll match l {
+        #l.#f. unroll match l {
             _u => { ret roll inj1 () }
             c => {
                 let (h, t) = { ret c }
-                let t2 = {{force filter} f t}
+                let t2 = {{force filter} t f}
                 if {{force f} h} {
                     {{force cons} h t2}
                 } else {
@@ -52,13 +52,13 @@ pub fn listing0 () { fgi_listing_test![
     }
 
     let rec map_filter:(
-        Thk[0] 0 (Thk[0] 0 Nat -> 0 F OpNat) -> 0 List -> 0 F List
+        Thk[0] 0 List -> 0 (Thk[0] 0 Nat -> 0 F OpNat) -> 0 F List
     ) = {
-        #f.#l. unroll match l {
+        #l. #f. unroll match l {
             _u => { ret roll inj1 () }
             c => {
                 let (h, t) = { ret c }
-                let t2 = {{force map_filter} f t}
+                let t2 = {{force map_filter} t f}
                 let oh2 = {{force f} h}
                 match oh2 {
                     _u => { ret t2 }
@@ -68,5 +68,34 @@ pub fn listing0 () { fgi_listing_test![
         }
     }
 
+    let rec reverse:(
+        Thk[0] 0 List -> 0 List -> 0 F List
+    ) = {
+        #l.#r. unroll match l {
+            _u => { ret r }
+            c => {
+                let (h, t) = { ret c }
+                let r2 = {{force cons} h r}
+                {{force reverse} t r}
+            }
+        }
+    }
+
+    let rec fold:(
+        Thk[0] 0 List ->
+            0 Nat ->
+            0 (Thk[0] 0 Nat -> 0 Nat -> 0 F Nat) ->
+            0 F Nat
+    ) = {
+        #l.#a.#f. unroll match l {
+            _u => { ret a }
+            c => {
+                let (h, t) = { ret c }
+                let a2 = {{force f} a h}
+                {{force fold} t a2 f}
+            }
+        }
+    }
+    
     ret 0
 ]}
