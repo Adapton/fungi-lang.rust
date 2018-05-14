ddd3ba587d8f66be3ad8d15dbeb9feee2d9b18db
unknown <jpw234@cornell.edu>
2018-05-14 11:30:50 -0600

    initial commit of streams

---
+++ src/examples/pure_stream_nat.rs
@@ -0,0 +1,41 @@
+#[test]
+pub fn listing0 () { fgi_listing_test![
+    decls {
+        //Nat stream
+        type Stream = (+ Unit + (x Nat x Thk Stream));
+
+        //Optional natural numbers
+        type OpNat = (+ Unit + Nat);
+    }
+
+    let nil:(
+        Thk[0] 0 F Stream
+    ) = {
+        ret thunk ret roll inj1 ()
+    }
+
+    let cons:(
+        Thk[0] 0 Nat -> 0 Stream -> 0 F Stream
+    ) = {
+        ret thunk #h.#t. ret roll inj2 (h, thunk ret t)
+    }
+
+    /*let rec map:(
+        Thk[0]
+            0 (Thk[0] 0 Nat -> 0 F Nat) ->
+            0 Stream ->
+            0 F Stream
+    ) = {
+        #f.#s. unroll match s {
+            _u => { ret roll inj1 () }
+            c => {
+                let (h, _t) = { ret c }
+                let h2 = {{force f} h}
+                let t2 = {{force map} f {force _t}}
+                {{force cons} h2 t2}
+            }
+        }
+    }*/
+
+    ret 0
+]}
